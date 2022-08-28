f8e5c8831d71d69a0ae7638b5bd93a95c1485c4e
Uttam Rao <uttamrao2000@gmail.com>
2022-08-28 18:43:17 +0530

    slide window technique

---
+++ Arrays/SlidingWindowTech.java
@@ -0,0 +1,15 @@
+public class SlidingWindowTech {
+    public static void main(String[] args) {
+        int arr []={1,2,3,4,5,6,7,8,9};
+        int k=3;
+        int subArrayCount=0;
+        for(int i=0;i<k;i++){
+            subArrayCount++;
+        }
+        System.out.println(subArrayCount);
+        for(int i=k;i<=arr.length-k;i++){
+            subArrayCount++;
+        }
+        System.out.println(subArrayCount);
+    }
+}
---
+++ Arrays/SubArraySum.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.ForkJoinPool;
+
+//Sliding window technique
+public class SubArraySum {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,4,5,6,7,8,9};
+        int k=3;
+        int sum=0;
+        for (int i = 0; i < k; i++) {
+            sum=sum+arr[i];       
+        }//1+2+3
+        System.out.println(sum);
+        for(int i=k;i<arr.length;i++){
+            sum=sum-arr[i-k]+arr[i];
+            System.out.println(sum);
+        }
+    }
+}
