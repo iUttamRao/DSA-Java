83263da941c7c47073d0fa4b3b2a710aa6354d4f
Uttam Rao <uttamrao2000@gmail.com>
2022-08-29 17:30:52 +0530

    print palindrome substring

---
+++ Strings/PrintPalindromeSubString.java
@@ -0,0 +1,27 @@
+public class PrintPalindromeSubString {
+    static boolean isPalindrome(String str){
+        int i=0;
+        int j=str.length()-1;
+        while(i<=j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str= "abccbc";
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                String temp=str.substring(i,j);
+                //System.out.print(temp+" ");
+                if(isPalindrome(temp)){
+                    System.out.println("Palindrome Substring "+temp);
+                }
+            }
+        }
+    }
+}
