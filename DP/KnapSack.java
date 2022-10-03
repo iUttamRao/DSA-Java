87957b779fdcc4b3eedca5062f387d5efe28e9e1
Uttam Rao <uttamrao2000@gmail.com>
2022-10-03 19:04:54 +0530

    Some changes

--- DP/KnapSack.java
+++ DP/KnapSack.java
@@ -1,5 +1,29 @@
 public class KnapSack {
 
+    static int tabulation(int prices[],int weights[],int maxWeight,int totalItems){
+        int matrix[][]= new int[totalItems+1][maxWeight+1];
+        //fill the 0th Row and fill the First column for the base case
+        for(int i=0;i<=maxWeight;i++){
+            matrix[0][i]=0; // Not need for java
+        }
+        for(int i=0;i<=totalItems;i++){
+            matrix[i][0]=0; // Not need for java
+        }
+        for(int i=1;i<=totalItems;i++){
+            for(int j=1;j<=maxWeight;j++){
+                if(weights[i-1]>j){ //given weight is > than the max weight(Col)
+                    matrix[i][j]=matrix[i-1][j];
+                }
+                else{
+                    int option1 =prices[i-1]+matrix[i-1][j-weights[i-1]];
+                    int option2 = matrix[i-1][j];
+                    matrix[i][j]=Math.max(option1,option2);
+                }
+            }
+        }
+        return matrix[totalItems][maxWeight];
+    }
+
     static int knapSach01(int []weights,int prices[],int maxWeight,int index){
         //Termination Case
         if(index==weights.length || maxWeight==0){ // Bag Full
@@ -29,5 +53,7 @@ public class KnapSack {
         int weights[]={1,2,2};
        int maxProfit =knapSach01(weights, prices, maxWeight, 0);
        System.out.println(maxProfit);
+
+       System.out.println(tabulation(prices, weights, maxWeight, prices.length));
     }
 }
